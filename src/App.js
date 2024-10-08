4be2e11163bfb2e77462bca5e4d0dc2ac40c8451
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-10-08 23:58:47 +0100

    added course and images

--- src/App.js
+++ src/App.js
@@ -10,23 +10,79 @@ new Vue({
                 location: "London",
                 price: 100,
                 spaces: 5,
-                image: 'https://via.placeholder.com/150'
+                image: '../images/calculator_12320345.gif'
             },
             {
                 id: 1002,
-                subject: "Music",
+                subject: "Maths",
                 location: "Cambridge",
                 price: 90,
                 spaces: 5,
-                image: 'https://via.placeholder.com/150'
+                image: '../images/calculator_17518098.gif'
             },
             {
                 id: 1003,
+                subject: "Maths",
+                location: "Oxford",
+                price: 120,
+                spaces: 5,
+                image: '../images/calculator_6454138.gif'
+            },
+            {
+                id: 1004,
                 subject: "English",
                 location: "Oxford",
-                price: 80,
+                price: 140,
                 spaces: 5,
-                image: 'https://via.placeholder.com/150'
+                image: '../images/dictionary_11935089.gif'
+            },
+            {
+                id: 1005,
+                subject: "English",
+                location: "London",
+                price: 160,
+                spaces: 5,
+                image: '../images/dictionary_11935089.gif'
+            },
+            {
+                id: 1006,
+                subject: "English",
+                location: "Cambridge",
+                price: 200,
+                spaces: 5,
+                image: '../images/dictionary_11935089.gif'
+            },
+            {
+                id: 1007,
+                subject: "Chemestry",
+                location: "Oxford",
+                price: 220,
+                spaces: 5,
+                image: '../images/chemistry_12035107.gif'
+            },
+            {
+                id: 1008,
+                subject: "Music",
+                location: "Oxford",
+                price: 230,
+                spaces: 5,
+                image: '../images/guitar_16431853.gif'
+            },
+            {
+                id: 1009,
+                subject: "Music",
+                location: "Cambridge",
+                price: 10,
+                spaces: 5,
+                image: '../images/guitar_16431853.gif'
+            },
+            {
+                id: 1010,
+                subject: "Music",
+                location: "London",
+                price: 300,
+                spaces: 5,
+                image: '../images/guitar_16431853.gif'
             }
         ],
         cart: [],  // Cart to store added products
@@ -58,6 +114,10 @@ new Vue({
                 });
                 this.cart = [];  // Empty the cart
             }
+        },
+
+        moveToCheckout(){
+            return true;
         }
     },
 
